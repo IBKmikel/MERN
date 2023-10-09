2ff730ba9849a5716db81fc8a84a62bec027153d
IBKmikel <mikel.ibkc@gmail.com>
2023-10-09 02:29:28 -0600

    creción de jwt

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const User = require('../models/user');
+const jwt = require('../utils/jwt');
 
 function register(req, res){
     const { firstname, lastname, email, password} = req.body;
@@ -29,7 +30,36 @@ function register(req, res){
     });
 }
 
+function login(req, res){
+    const {email, password} = req.body;
+    if(!email) res.status(400).send({msg: 'El email es obligatorio'});
+    if(!password) res.status(400).send({msg: 'La contraseña es obligatorio'});
+
+    const emailLowerCase = email.toLowerCase();
+    User.findOne({email: emailLowerCase}, (error, userStore) =>{
+        if(error){
+            res.status(500).send({msg: 'Error del servidor'});
+        }else{
+            bcrypt.compare(password, userStore.password, (bcryptError,check) =>{
+                if(bcryptError){
+                    res.status(500).send({msg: 'Error en el servidor'});
+                }else if(!check){
+                    res.status(400).send({msg: 'Error en el servidor'});
+                }else if(!userStore.active){
+                    res.status(401).send({msg: 'Usuario no autorizado o activo'});
+                }else{
+                    res.status(200).send({
+                        access: jwt.createAccessToken(userStore),
+                        refresh: jwt.createRefreshToken(userStore)
+                    });
+                }
+            });
+        }
+    });
+}
+
 
 module.exports = {
     register,
+    login
 }
